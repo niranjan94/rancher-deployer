dd5aaa621c58068c70b708d34c892903f7ab5ef6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-11 06:28:04 +0000

    Deploy niranjan94/rancher-deployer to github.com/niranjan94/rancher-deployer.git:gh-pages

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 
 RANCHER_DEPLOYER=$(mktemp)
 
-curl -L https://github.com/niranjan94/rancher-deployer/releases/download/1.0.2/rancher-deployer_$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m) -o ${RANCHER_DEPLOYER}
+curl -L https://github.com/niranjan94/rancher-deployer/releases/download/1.0.3/rancher-deployer_$(uname -s | tr '[:upper:]' '[:lower:]')_$(uname -m) -o ${RANCHER_DEPLOYER}
 chmod +x ${RANCHER_DEPLOYER}
 
 ${RANCHER_DEPLOYER} $@
